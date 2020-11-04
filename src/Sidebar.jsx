91896001b5b9a2a62d16fa75a4177537f2cc84f1
ubermanu <e.vodor@gmail.com>
2020-11-04 02:12:06 +0100

    Update style, create Toast component and remove react logo

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Sidebar.scss'
 
 function Sidebar() {
   return (
---
+++ src/Toast.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Toast() {
+  return (
+    <div id="kfToast">
+      Click on an element you would like to animate.
+    </div>
+  )
+}
+
+export default Toast
