1436cf8a0a47bd0a9a340adb6383c65f77008ebc
ubermanu <e.vodor@gmail.com>
2020-11-04 12:16:12 +0100

    Move extension resources into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 
 module.exports = {
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
+    filename: 'extension.js',
     publicPath: '/',
   },
   module: {
@@ -22,6 +23,11 @@ module.exports = {
   },
   mode: 'development',
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),
