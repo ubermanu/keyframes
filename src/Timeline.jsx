331b1a75bd9cab70e0b79844ed6e32e18ac9721c
ubermanu <e.vodor@gmail.com>
2020-11-04 20:15:46 +0100

    Update timeline component

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,44 +1,49 @@
 import React from 'react'
 
+function AnimationOption({ label, children }) {
+  return (
+    <div className="kftn-animation-prop">
+      <label>{label}</label>
+      {children}
+    </div>
+  )
+}
+
 function Timeline() {
   return (
     <div className="kf-timeline">
-
       <div className="kf-timeline-nav">
-
         <div className="kftn-left">
           <button className="kf-btn" id="kfStartAnimationButton">
             Start Animation
           </button>
-
           <button className="kf-btn red" id="kfStopAnimationButton">
             Stop Animation
           </button>
         </div>
 
         <div className="kftn-mid">
-          <div className="kftn-animation-prop">
-            <label>Animation<br />Duration</label>
-            <input type="text" value="3s" id="animationDuration" />
-          </div>
-          <div className="kftn-animation-prop">
-            <label>Iterations</label>
-            <input type="text" value="infinite" id="animationIterations" />
-          </div>
-          <div className="kftn-animation-prop">
-            <label>Delay</label>
-            <input type="text" value="0s" id="animationDelay" />
-          </div>
-          <div className="kftn-animation-prop">
-            <label>Timing</label>
-            <select id="animationTiming">
+          <AnimationOption label={<span>Animation<br />Duration</span>}>
+            <input name="duration" type="text" value="3s" />
+          </AnimationOption>
+
+          <AnimationOption label="Iterations">
+            <input name="iterations" type="text" value="infinite" />
+          </AnimationOption>
+
+          <AnimationOption label="Delay">
+            <input name="delay" type="text" value="0s" />
+          </AnimationOption>
+
+          <AnimationOption label="Timing">
+            <select name="timing">
               <option value="linear">Linear</option>
               <option value="ease">Ease</option>
               <option value="ease-in">Ease-In</option>
               <option value="ease-out">Ease-Out</option>
               <option value="ease-in-out">Ease-In-Out</option>
             </select>
-          </div>
+          </AnimationOption>
         </div>
 
         <div className="kftn-right">
@@ -58,7 +63,8 @@ function Timeline() {
 
       <div className="timeline-footer">
         <span>Click Timeline to add step</span>
-        <span className="small">Made by <a href="https://mitch.works" target="_blank">mitch.works</a> / <a
+        <span className="small">Made by <a href="https://mitch.works"
+                                           target="_blank">mitch.works</a> / <a
           href="https://twitter.com/sleumasm"
           target="_blank">@sleumasm</a></span>
         <span id="closeKeyframes">Close Keyframes.app</span>
