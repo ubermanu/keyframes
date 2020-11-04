3580f48d13766fe45cc3cf899f81b4c04faae49f
ubermanu <e.vodor@gmail.com>
2020-11-04 18:57:50 +0100

    Animate toast

--- src/Toast.jsx
+++ src/Toast.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import $ from 'jquery'
 
 function Toast({ message }) {
+  const divEl = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      $(divEl.current).animate({
+        left: '0px',
+      }, 250, $.noop)
+    }, 100)
+
+    setTimeout(() => {
+      $(divEl.current).animate({
+        left: '-240px',
+      }, 250, $.noop)
+    }, 4000)
+  }, [])
+
   return (
-    <div id="kfToast">{message}</div>
+    <div id="kfToast" ref={divEl}>{message}</div>
   )
 }
 
