a761892ba5bfd50a204751de7c2e096b997f9970
ubermanu <e.vodor@gmail.com>
2020-11-04 02:03:30 +0100

    Add sidebar

---
+++ src/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import './Sidebar.scss'
+
+function Sidebar() {
+  return (
+    <div className="App-Sidebar">
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default Sidebar
