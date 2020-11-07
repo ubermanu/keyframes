208178da0248de580318648483344a52a0a840bd
ubermanu <e.vodor@gmail.com>
2020-11-07 08:59:52 +0100

    Render styles

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -31,53 +31,54 @@ function Sidebar() {
 
         <PresetGroup title="Transform">
           <Preset label="Rotate" type="text" placeholder="180deg"
-                  name="transformRotate" onChange={handleChange} />
+                  name="rotate" onChange={handleChange} />
           <Preset label="Scale" type="number" step="0.1" placeholder="1.5"
-                  name="transformScale" onChange={handleChange} />
+                  name="scale" onChange={handleChange} />
           <Preset label="TranslateX" type="text" placeholder="100px"
-                  name="transformX" onChange={handleChange} />
+                  name="translateX" onChange={handleChange} />
           <Preset label="TranslateY" type="text" placeholder="-20%"
-                  name="transformY" onChange={handleChange} />
+                  name="translateY" onChange={handleChange} />
           <Preset label="SkewX" type="text" placeholder="15deg"
-                  name="transformSkewX" onChange={handleChange} />
+                  name="skewX" onChange={handleChange} />
           <Preset label="SkewY" type="text" placeholder="-20deg"
-                  name="transformSkewY" onChange={handleChange} />
+                  name="skewY" onChange={handleChange} />
           <Preset label="Transform Origin" type="text" placeholder="50% 50%"
-                  name="transformOrigin" onChange={handleChange} />
+                  name="transform-origin" onChange={handleChange} />
         </PresetGroup>
 
         <PresetGroup title="Colors & Font">
           <Preset label="Background" type="text" placeholder="#a5c9e4"
-                  onChange={handleChange} />
+                  name="background" onChange={handleChange} />
           <Preset label="Opacity" type="number" max="1" min="0"
-                  step="0.01" placeholder="0.5" onChange={handleChange} />
+                  step="0.01" placeholder="0.5"
+                  name="opacity" onChange={handleChange} />
           <Preset label="Color (Text)" type="text" placeholder="#f0f99c"
-                  onChange={handleChange} />
+                  name="color" onChange={handleChange} />
           <Preset label="Font Size" type="text" placeholder="18px"
-                  onChange={handleChange} />
+                  name="font-size" onChange={handleChange} />
           <Preset label="Font Weight" type="text" placeholder="bolder"
-                  onChange={handleChange} />
+                  name="font-weight" onChange={handleChange} />
         </PresetGroup>
 
         <PresetGroup title="Size & Spacing">
           <Preset label="Width" type="text" placeholder="100px"
-                  onChange={handleChange} />
+                  name="width" onChange={handleChange} />
           <Preset label="Height" type="text" placeholder="120px"
-                  onChange={handleChange} />
+                  name="height" onChange={handleChange} />
           <Preset label="Margin" type="text" placeholder="5px"
-                  onChange={handleChange} />
+                  name="margin" onChange={handleChange} />
           <Preset label="Padding" type="text" placeholder="10px"
-                  onChange={handleChange} />
+                  name="padding" onChange={handleChange} />
         </PresetGroup>
 
         <PresetGroup title="Border & Shadow">
           <Preset label="Border" type="text" placeholder="5px solid red"
-                  onChange={handleChange} />
+                  name="border" onChange={handleChange} />
           <Preset label="Box Shadow" type="text"
                   placeholder="10px 10px 5px 2px rgba(0,0,0,0.75)"
-                  onChange={handleChange} />
+                  name="box-shadow" onChange={handleChange} />
           <Preset label="Outline" type="text" placeholder="2px solid red"
-                  onChange={handleChange} />
+                  name="outline" onChange={handleChange} />
         </PresetGroup>
 
         <button className="kf-btn red small full center" onClick={handleDelete}>
--- src/StylesRenderer.jsx
+++ src/StylesRenderer.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { useStore } from './store'
 
+const transformProps = [
+  'rotate',
+  'scale',
+  'translateX',
+  'translateY',
+  'skewX',
+  'skewY',
+]
+
 function StylesRenderer() {
   const { state } = useStore()
 
@@ -10,12 +19,34 @@ function StylesRenderer() {
   animationProperties += state.animation.delay + ' '
   animationProperties += state.animation.timing + ';'
 
+  function getStepStyles(styles) {
+    let css = {}
+
+    for (let propertyName in styles) {
+      if (styles.hasOwnProperty(propertyName)) {
+        const value = styles[propertyName]
+        if (transformProps.includes(propertyName)) {
+          if (!('transform' in css)) {
+            css['transform'] = ''
+          }
+          css['transform'] += ' ' + propertyName + '(' + value + ')'
+        } else {
+          css[propertyName] = value
+        }
+      }
+    }
+
+    return Object.entries(css).map(([property, value]) => `
+      ${property}: ${value};
+    `).join('')
+  }
+
   function getElementStyles() {
     return `
     @keyframes yourAnimation {
-    ${state.steps.map(({ id, styles }) => {
-      // return id + '%{' + styles.map((k, v) => k + ':' + v + ';') + '}'
-    })}
+    ${state.steps.map(({ id, styles }) => `${id}% {
+        ${getStepStyles(styles)}
+      }`)}
     }
     .elementToAnimate {
       animation: yourAnimation ${animationProperties}
